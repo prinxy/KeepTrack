131e9035b909b6e7ca4e33d02611f697e0813a9f
Muhammad Shehriyar Qureshi <S.Shehriyar266@gmail.com>
2017-05-24 00:07:18 +0500

    Place Manipulation are half done

--- keeptrack.py
+++ keeptrack.py
@@ -1,126 +1,156 @@
-class Item:
+import sys
+
+
+class Thing:
 
     room = []
 
-    def __init__(self, item_name):
-        self.item_name = item_name
+    def __init__(self, thing_name):
+        self.thing_name = thing_name
 
-    def additem(self, item_name):
-        item_location = str.lower(input("Location: "))
-        Creation.location(item_location)
+    def add_thing(self, thing_name):
+        thing_place = str.lower(input("Place: "))
+        Creation.place(thing_place)
 
-        userinput = str.lower(input("Add more? y/n "))
+        user_input = str.lower(input("Add more? y/n "))
 
-        if userinput == "y":
-            Creation.object()
+        if user_input == "y":
+            Creation.thing()
 
-        elif userinput == "n":
+        elif user_input == "n":
             main()
 
         else:
             print("Wrong Selection")
             main()
 
-    def removeitem():
+    def remove_thing():
         pass
-
         # print(Item.room)  # check
         # item_name = str.lower(input("Input name of item: "))
-        # item_location = str.lower(input("Location: "))
-        # userdata = open("locations.txt", "r")
+        # item_place = str.lower(input("place: "))
+        # userdata = open("places.txt", "r")
         # userdata.read()
         # # with open(filename) as myFile:
 
         # else:
         #     print("Item does not exist")
-        #     Item.removeitem()
+        #     Item.remove_item()
 
-    def removelocation():
-        userinput = str.lower(input("Location: "))
-        if userinput in Item.room:
-            Item.room.pop(userinput)
-            print(Item.room)
+    def remove_place():
+        user_input = str.lower(input("Place: "))
+        if user_input in Thing.room:
+            Thing.room.remove(user_input)
+            print(Thing.room)
             main()
 
-        elif userinput not in Item.room:
-            print("Location does not exist")
+        elif user_input not in Thing.room:
+            print("place does not exist")
             main()
 
     @staticmethod
-    def viewitems():
-        # print(Item.room)
-        # print(Item.room) # check
-        main()
+    def view_things():
+        pass
+        # main()
 
     @staticmethod
-    def viewlocations():
-        with open("locations.txt", "r") as userdata:
-            locations = userdata.readlines()
-            locations = [x.strip() for x in locations]
-            for location in locations:
-                print(location[:-2])
-
+    def view_places():
+        print(Thing.room)
         main()
 
 
 class Creation():
 
-    def object():  # Object should have 2 values, name and location
-        item_name = str.lower(input("Item name: "))
-        item = Item(item_name)
-        item.additem(item_name)
+    def thing():  # Object should have 2 values, name and place
+        thing_name = str.lower(input("Thing name: "))
+        thing = Thing(thing_name)
+        thing.add_thing(thing_name)
 
-    def location(item_location):
-        # item_location = str.lower(input("New Location: "))
-        if item_location in Item.room:
+    def place(thing_place):
+        # item_place = str.lower(input("New place: "))
+        if thing_place in Thing.room:
             print("In room")
 
-        elif item_location not in Item.room:
-            pass
-            # Item.room.append(item_location)
-            # location_index = Item.room.index(item_location)
-            # print(location_index)
-            # with open("locations.txt", "a") as userlocations:
-            #     userdata.write("{}-{} \n".format(location_index, ))
-            #     userdata.close()
+        elif thing_place not in Thing.room:
+            Thing.room.append(thing_place)
+            place_index = Thing.room.index(thing_place)
+            print(place_index)
+            with open("places.txt", "a") as user_places:
+                user_places.write("{}-{}\n".format(thing_place, place_index))
+                main()
+
         else:
             pass
+
+    def place_with_location():
+        pass
 
 
 def main():
 
     # Check file for data and conditionalize as needed
-    if Item.room == []:
-        with open("locations.txt", "r") as userdata:
-            locations = userdata.readlines()
-            locations = [x.strip() for x in locations]
-            print(locations)
+    if Thing.room == []:
+        with open("places.txt", "r") as load_places:
+            places = load_places.readlines()
+            # place = [place.strip() for place in places]
+            for place in places:
+                # print(place[:-3])
+                # print(place)
+                Thing.room.append(place[:-3])
+            print(Thing.room)
+
+        # with open("things.txt", "r") as load_things:
+        #     things = load_things.readlines()
+        #     # thing = [thing.strip() for thing in things]
+        #     for thing in things:
+        #         # list_things = things[2:]
+        #         print(thing[:-3])
+        #         # if thing[-1] == 0:
+        #         Thing.room[0].append(thing)
+        #         print(Thing.room[0])
+        #     # elif thing[0] == 1:
+        #     # pass
+
+    # print("""
+    # (A)dd (I)tem, (A)dd (L)ocation, (R)emove (I)tem, (R)emove (L)ocation
+    # (V)iew (I)tems or (V)iew (L)ocations or (Q)uit
+    # """)
 
     print("""
-    (A)dd (I)tem, (A)dd (L)ocation, (R)emove (I)tem, (R)emove (L)ocation
-    (V)iew (I)tems or (V)iew (L)ocations
+1. Add Things
+2. Remove Things
+3. View Things
+4. Add Places
+5. Remove Places
+6. View Places
+7. Exit
     """)
 
-    userinput = str.lower(input("Enter Selection: "))
+    user_input = str(input("Enter Selection: "))
 
-    if userinput == "ai":
-        Creation.object()
+    if user_input == '1':
+        Creation.thing()
 
-    elif userinput == "ri":
-        Item.removeitem()
+    elif user_input == '2':
+        Thing.remove_thing()
 
-    elif userinput == "rl":
-        Item.removelocation()
+    elif user_input == '3':
+        Thing.view_thing()
 
-    elif userinput == "vi":
-        Item.viewitems()
+    elif user_input == '4':
+        thing_place = str.lower(input("Place: "))
+        Creation.place(thing_place)
 
-    elif userinput == "vl":
-        Item.viewlocations()
+    elif user_input == '5':
+        Thing.remove_place()
 
-    elif userinput == "al":
-        item_location = str.lower(input("Location: "))
-        Creation.location(item_location)
+    elif user_input == '6':
+        Thing.view_places()
+
+    elif user_input == '7':
+        # pass
+        # Add save file method and the end bro
+        sys.exit()
 
     else:
         print("Wrong selection")

37d3a6301279db1c19fe547c6d6af5e7b0fb2cf3
Shehriyar Qureshi <SShehriyar266@gmail.com>
2017-06-06 13:10:34 +0500

    Adding items to be manipulated in a list

--- keeptrack.py
+++ keeptrack.py
@@ -5,8 +5,9 @@ import sys
 class Thing:
 
     room = []  # create empty list named room
-
+    user_items = []
     # initilize object creation values
+
     def __init__(self, thing_name):
         self.thing_name = thing_name
 
@@ -27,49 +28,27 @@ class Thing:
             main()
 
     def remove_thing():
-        pass
-        # print(Item.room)  # check
-        # item_name = str.lower(input("Input name of item: "))
-        # item_place = str.lower(input("place: "))
-        # userdata = open("places.txt", "r")
-        # userdata.read()
-        # # with open(filename) as myFile:
-
-        # else:
-        #     print("Item does not exist")
-        #     Item.remove_item()
+        to_remove = str.lower(input("To remove: "))
+        if to_remove in Thing.user_items:
+            the_index = Thing.user_items.index(to_remove)
+            Thing.user_items.pop(the_index)
+            main()
+        else:
+            pass
 
     def remove_place():
-        pass
-        # user_input = str.lower(input("Place: "))
-        # if user_input in Thing.room:
-        #     # place_index = Thing.room.index(user_input)
-        #     Thing.room.remove(user_input)
-        #     # remove_thing()
-        #     with open("things.txt", "r") as load_things:
-        #         things = load_things.readlines()
-        #         thing = [thing.strip() for thing in things]
-        #         # if place_index in thing:
-        #     print(Thing.room)
-        #     main()
-        #
-        # elif user_input not in Thing.room:
-        #     print("place does not exist")
-        #     main()
+        user_input = str.lower(input("Place: "))
+        if user_input in Thing.room:
+            place_index = Thing.room.index(user_input)
+            Thing.room.pop(place_index)
+            with open("places.txt", "r") as load_things:
+                things = load_things.readlines()
+                thing = [thing.strip() for thing in things]
+                print(thing)
 
     @staticmethod
     def view_things():
-        with open("things.txt", "r") as load_things:
-            things = load_things.readlines()
-            thing = [thing.strip() for thing in things]
-            print(thing)
-            for each_thing in thing:
-                for i in range(len(Thing.room)):
-
-                    if each_thing[-1] == '{}'.format(i):
-                        print("{} belongs"
-                              + "to {}".format(each_thing, Thing.room[i]))
-
+        print(Thing.user_items)
         main()
 
     @staticmethod
@@ -104,15 +83,28 @@ class Creation():
     def item_with_location(thing_name, thing_place):
         if thing_place in Thing.room:
             place_index = Thing.room.index(thing_place)
-            print(place_index)
-            with open("things.txt", "a") as user_things:
-                user_things.write("{}-{}\n".format(thing_name, place_index))
+            Thing.user_items[place_index].append(thing_name)
+            print(Thing.room)
+            print(Thing.user_items)
+            # place_index = Thing.room.index(thing_place)
+            # print(place_index)
+            # with open("things.txt", "a") as user_things:
+            #     user_things.write("{}-{}\n".format(thing_name, place_index))
 
         elif thing_place not in Thing.room:
-            Creation.place(thing_place)
+            # Append the place to list room
+            Thing.room.append(thing_place)
             place_index = Thing.room.index(thing_place)
-            with open("things.txt", 'a') as user_things:
-                user_things.write("{}-{}\n".format(thing_name, place_index))
+            try:
+                Thing.user_items[place_index].append(thing_name)
+            except IndexError:
+                Thing.user_items.insert(place_index, [])
+                Thing.user_items[place_index].append(thing_name)
+            print(Thing.room)
+            print(Thing.user_items)
+
+            # Creation.place(thing_place)
+            # place_index = Thing.room.index(thing_place)
 
         else:
             pass
@@ -121,7 +113,7 @@ class Creation():
 def main():
 
     # Check file for data and conditionalize as needed
-    if Thing.room == []:
+    if Thing.room == [] and Thing.user_items == []:
         with open("places.txt", "r") as load_places:
             places = load_places.readlines()
             for place in places:
@@ -129,20 +121,17 @@ def main():
             print(Thing.room)
 
             # we now have a list named room having places as values
-        #
-        # with open("things.txt", "r") as load_things:
-        #     things = load_things.readlines()
-        #     thing = [thing.strip() for thing in things]
-        #     # the_things = things.split('\n')
-        # print(thing)
-        # # print(thing[1])
-        # # total_places = len(Thing.room)
-        # for each_thing in thing:
-        #     for i in range(len(Thing.room)):
-        #
-        #         if each_thing[-1] == '{}'.format(i):
-        #             print("{} belongs"
-        #                   + "to {}".format(each_thing, Thing.room[i]))
+
+        with open("things.txt", "r") as load_things:
+            the_things = load_things.readlines()
+            thing = [thing.strip() for thing in the_things]
+            for i in range(len(Thing.room)):
+                Thing.user_items.insert(i, [])
+                for each_thing in thing:
+                    print('{}'.format(each_thing[-1]))
+                    if each_thing[-1] == '{}'.format(i):
+                        Thing.user_items[i].append(each_thing[:-2])
+            print(Thing.user_items)
 
     print("""
 1. Add Things
@@ -178,13 +167,32 @@ def main():
 
     elif user_input == '7':
         # pass
-        # Add save file method and the end bro
-        sys.exit()
+        with open("places.txt", 'w') as place_in_file:
+            for each_place in Thing.room:
+                place_index = Thing.room.index(each_place)
+                place_in_file.write("{}-{}\n".format(each_place,
+                                                     place_index))
+
+        with open("things.txt", 'w') as thing_to_file:
+            for each_thing in Thing.user_items:
+                length = len(each_place)
+                if length > 0:
+                    for one_thing in each_thing:
+                        print(one_thing)
+                        thing_index = each_thing.index(one_thing)
+                        thing_to_file.write("{}-{}\n".format(one_thing,
+                                                             thing_index))
+
+                elif length == 0:
+                    pass
+                else:
+                    pass
+
+                sys.exit()
 
     else:
         print("Wrong selection")
         main()
 
 
-if __name__ == '__main__':
-    main()
+main()
